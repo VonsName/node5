2ff9571ca2af11ed2aa4d0f07d9d42164de50b13
fjl <fengjianglin6817@dingtalk.com>
2018-05-30 17:27:42 +0800

    修改

--- index.js
+++ index.js
@@ -4,12 +4,19 @@ var app=express();
 /*app.get('/', function (req, res) {
     res.send('Hello World');
 });*/
-app.use('/',function (req,res) {
+/*app.use('/',function (req,res) {
     //res.write({a:12,b:23});//First argument must be a string or Buffer
     res.send({a:12,b:23});
     res.end();
-});
+});*/
+app.route('/user')
+    .get(function (req,res) {
+       res.send('get请求')
+    });
 
+app.route('/').post(function (req,res) {
+    res.send('post请求');
+});
 var server = app.listen(8081, function () {
 
     var host = server.address().address;
